7eb33b80e7f417515c740ffce8dfb3113a1f6fb7
YuyaOkumura <yuya.okumura.majime@gmail.com>
2017-08-10 17:48:23 +0900

    catch header valiable

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,28 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make acomponent
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
-  return(
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Albums!</Text>
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#f8f8f8'
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
